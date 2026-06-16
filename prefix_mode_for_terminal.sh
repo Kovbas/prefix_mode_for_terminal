8fb69eafd5f115313e5e456770e970a8753ac97d
Valentin Kovbas <valentin.kovbas@zenseact.com>
2026-06-16 20:29:59 +0000

    Add NO PREFIX and clean exit

--- prefix_mode_for_terminal.sh
+++ prefix_mode_for_terminal.sh
@@ -1,20 +1,31 @@
 #!/bin/bash -i
 
+stty -echoctl
+
+exit_on() {
+    printf "\n"
+    exit
+}
+
+trap exit_on SIGINT
+
 PREFIX=$1
 
 FMT_RED="\033[0;31m"
+FMT_GREEN="\033[0;32m"
 FMT_BOLD="\033[1m"
 FMT_RESET="\033[0m"
 
+if [ -z "${PREFIX}" ]
+then
+    PRMPT="${PS1@P}${FMT_RED}${FMT_BOLD}NO PREFIX${FMT_RESET}> "
+else
+    PRMPT="${PS1@P}prefix ${FMT_GREEN}${FMT_BOLD}${PREFIX}${FMT_RESET}> "
+fi
+
 while  :; do
-    printf "${PS1@P}prefix ${FMT_RED}${FMT_BOLD}${PREFIX}${FMT_RESET}> "
+    printf "${PRMPT}"
     read -ra command
+    if [ ${command} == "exit" ]; then break; fi
     ${PREFIX} "${command[@]}"
 done
-
-# while read -erp "*git*${PS1@P}" cmd rest; do
-#         if _=`git help $cmd 2>&-`
-#                 then eval git $cmd "$rest"
-#                 else eval $cmd "$rest"
-#         fi
-# done
